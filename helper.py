d93525f638a2f99f7db3f0d695f660a391c02a5d
Nathan Haight <nathanjohnhaight@gmail.com>
2025-09-28 13:41:45 -0600

    Gets the coorect words dpending on the psotions of known words

--- helper.py
+++ helper.py
@@ -1,29 +1,47 @@
-from words import words
-
-potential_words = []
-current_info = ["_"] * 5
-
-print("How to use:\nFirst enter the word. \n\x1b[1;32mThen enter the known letters positions (1-5) separated by spaces. \x1b[0m \n\x1b[1;33mThen enter the letters that are in the word but not whose positions are not known\x1b[0m \nEnter the word 'exit' to quit the program.")
+from words import WORDS
 
 def main():
+    current_info = ["_"] * 5
+    potential_words = list(WORDS)
+
+    print("How to use:\nFirst enter the word. \n\x1b[1;32mThen enter the known letters positions (1-5) separated by spaces. \x1b[0m \n\x1b[1;33mThen enter the letters that are in the word but not whose positions are not known\x1b[0m \nEnter the word 'exit' to quit the program.")
+
     while True:
         word = input("Enter your 5 letter word:\n").strip()
         if len(word) == 5:
-            correct_position(word, input("Enter the position of the known letters:\n"))
+            positions = input("Enter the position of the known letters:\n")
+            if len(positions) > 0:
+                current_info, potential_words = correct_position(current_info, potential_words, word, positions)
             print(current_info)
+            print(potential_words)
         elif word == "exit":
             break
         else:
             print("Your word must be a five letters long")
 
-def correct_position(word, positions):
+def correct_position(current_info, potential_words, word, positions):
     positions = map(int, positions.split(" "))
+    buffer_info = current_info
+    buffer_list = []
 
     for position in positions:
-        letter = word[position - 1]
-        # for w in words:
-        #     print(w)
-        #     if letter == w[position - 1]:
+        if buffer_info[position - 1] == "_":
+            buffer_info[position - 1] = word[position - 1]
+        else:
+            print(f"{position} already has a confirmed letter")
+            break
+
+    for w in potential_words:
+        w_list = list(w)
+        for i in range(5):
+            char = buffer_info[i]
+            if char !=  "_":
+                w_list[i] = buffer_info[i]
+
+        if list(w) == w_list:
+            buffer_list.append(w)
+
+    return buffer_info, buffer_list  
                  
     
 
--- words.py
+++ words.py
@@ -1,4 +1,4 @@
-words = ("aback",
+WORDS = ("aback",
 "abaft",
 "abase",
 "abate",
